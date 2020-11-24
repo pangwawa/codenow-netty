d656b4ddf932f58066448f592acec7f912e54b4f
Jack Wu <wuj@es-display.com>
2020-11-24 18:13:03 +0800

    netty socket

---
+++ netty-socket/src/main/java/fun/codenow/netty/socket/NettySocketApplication.java
@@ -0,0 +1,17 @@
+package fun.codenow.netty.socket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author Jack Wu
+ * @Description
+ * @Version V1.0
+ * @Date2020/11/24 18:03
+ **/
+@SpringBootApplication
+public class NettySocketApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NettySocketApplication.class);
+    }
+}
